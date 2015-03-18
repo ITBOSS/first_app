977cd0e7399f24af64f20dcc277dfe5ebb6c773a
ITBOSS <git@itbose.com>
2015-03-18 17:12:10 +0900

    boardテーブル追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,18 @@
 class BoardsController < ApplicationController
   def index
   end
+
   def new
+    @boards = Board.new
+  end
+
+  def create
+    @board = Board.new(params_board)
+    @board.save
+    redirect_to "/boards"
+  end
+
+  def params_board
+    params.permit(:title, :editor)
   end
 end
